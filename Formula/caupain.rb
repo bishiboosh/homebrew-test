f196979ae5190d58bc3170ff4ece4a2f7c02f676
Valentin Rocher <vrocher@deezer.com>
2025-05-07 09:00:37 +0200

    Update caupain to 0.2.0

--- Formula/caupain.rb
+++ Formula/caupain.rb
@@ -1,15 +1,9 @@
 class Caupain < Formula
   desc "Your best buddy for keeping versions catalogs up to date!"
   homepage "https://github.com/bishiboosh/caupain"
-  url "https://github.com/bishiboosh/caupain/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9bbd11ce5fde4ccc9d645f340e2f11c305bbb377de9b9a6dfc87d2a69feea61f"
+  url "https://github.com/bishiboosh/caupain/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "66c590f573dca5ce2a5451b850165f89717622436d2836b0cb23fbaecb163a5f"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/bishiboosh/homebrew-test/releases/download/caupain-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8eeef011accd3410ca4a1c9332b77a88f8497cb048942a707fd30a5fef762af4"
-    sha256 cellar: :any_skip_relocation, ventura:       "83343d0aafc789de402c13eb03e7fd9b5819d168cbc06bedf4683beeb9781784"
-  end
 
   depends_on "openjdk@17" => :build
 
@@ -26,7 +20,7 @@ class Caupain < Formula
       else
         ":cli:macosX64Binaries"
       end
-    system "./gradlew", task
+    system "./gradlew", task, "--no-configuration-cache"
     folder =
       if Hardware::CPU.arm?
         "macosArm64"
@@ -34,12 +28,18 @@ class Caupain < Formula
         "macosX64"
       end
     bin.install "cli/build/bin/#{folder}/releaseExecutable/caupain.kexe" => "caupain"
-    # bash_completion.install "cli/completions/bash-completions.sh" => "caupain"
-    # fish_completion.install "cli/completions/fish-completions.sh"
-    # zsh_completion.install "cli/completions/zsh-completions.sh" => "_caupain"
+    bash_completion.install "cli/completions/bash-completion.sh" => "caupain"
+    fish_completion.install "cli/completions/fish-completion.sh"
+    zsh_completion.install "cli/completions/zsh-completion.sh" => "_caupain"
   end
 
   test do
-    system "#{bin}/caupain", "-h"
+    resource "testcatalog" do
+      url "https://raw.githubusercontent.com/bishiboosh/caupain/refs/heads/main/cli/tests/libs.versions.toml"
+      sha256 "0438c2ab962721021790e4fea4ead214b91c5019119593a801c9da562ae374ac"
+    end
+    resource("testcatalog").stage do
+      shell_output("#{bin}/caupain --verbose -i libs.versions.toml")
+    end
   end
 end
